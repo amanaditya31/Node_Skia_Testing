7cdf0736cbbc329f58cbad8ced4ea7adb5ca6533
amanaditya31 <aman01aditya@gmail.com>
2024-04-03 01:52:34 +0530

    added a  function to draw in test.mjs

--- test.mjs
+++ test.mjs
@@ -2,26 +2,24 @@ import { SkiaCanvas } from './index.js'
  
 // console.log('From native', sum(40, 2))
 
+function draw (){
+    let canvas = new SkiaCanvas(2560, 1280);
+    canvas.scale(1.2, 1.2);
+    canvas.moveTo(36.0, 48.0);
+    canvas.quadTo(660.0, 880.0, 1200.0, 360.0);
+    canvas.translate(10.0, 10.0);
+    canvas.setLineWidth(20.0);
+    canvas.stroke();
+    canvas.save();
+    canvas.moveTo(30.0, 90.0);
+    canvas.lineTo(110.0, 20.0);
+    canvas.lineTo(240.0, 130.0);
+    canvas.lineTo(60.0, 130.0);
+    canvas.lineTo(190.0, 20.0);
+    canvas.lineTo(270.0, 90.0);
+    canvas.fill();
+    canvas.save();
+    canvas.saveTo("test.png");
+}
 
-let canvas = new SkiaCanvas(2560, 1280);
-canvas.scale(1.2, 1.2);
-canvas.moveTo(36.0, 48.0);
-canvas.quadTo(660.0, 880.0, 1200.0, 360.0);
-canvas.translate(10.0, 10.0);
-canvas.setLineWidth(20.0);
-canvas.stroke();
-canvas.save();
-canvas.moveTo(30.0, 90.0);
-canvas.lineTo(110.0, 20.0);
-canvas.lineTo(240.0, 130.0);
-canvas.lineTo(60.0, 130.0);
-canvas.lineTo(190.0, 20.0);
-canvas.lineTo(270.0, 90.0);
-canvas.fill();
-canvas.save();
-canvas.saveTo("test.png");
-
-// let d = canvas.data();
-// let file = Filecreate("test.png").unwrap();
-// let bytes = d.as_bytes();
-// file.write_all(bytes).unwrap();.
+draw();
